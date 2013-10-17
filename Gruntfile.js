f4899dc765ab1749a9addc3caec29e5ebcdc0e9f
EaseWay Hu <easeway@gmail.com>
2013-10-17 21:13:56 +0800

    update incubator

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,12 @@
 module.exports = function (grunt) {
-    grunt.initConfig({});
-    grunt.loadTasks('tasks');
+    grunt.initConfig({
+        incubator: {
+            path: __dirname + '/packages'
+        }
+    });
+    grunt.loadNpmTasks('incubator');
+    grunt.registerTask('build-iso', 'incubate:isoimage');
+    grunt.registerTask('build-dev', 'incubate:roofs-dev');
+    grunt.registerTask('build-all', 'incubate:isoimage:rootfs-dev');
+    grunt.registerTask('default', 'build-iso');
 };
--- tasks/default.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (grunt) {
-    grunt.registerTask('build-iso', 'incubate:isoimage');
-    grunt.registerTask('build-dev', 'incubate:roofs-dev');
-    grunt.registerTask('build-all', 'incubate:isoimage:rootfs-dev');
-    grunt.registerTask('default', 'build-iso');
-};
